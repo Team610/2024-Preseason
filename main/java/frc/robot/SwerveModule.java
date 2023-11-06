21863dfaa853a95b7f5790b60e538605be2c93a2
David Shan <davidbshan@gmail.com>
2023-11-05 20:59:41 -0500

    added canbus name

--- main/java/frc/robot/SwerveModule.java
+++ main/java/frc/robot/SwerveModule.java
@@ -30,15 +30,15 @@ public class SwerveModule {
         this.angleOffset = moduleConstants.angleOffset;
         
         /* Angle Encoder Config */
-        angleEncoder = new CANCoder(moduleConstants.cancoderID);
+        angleEncoder = new CANCoder(moduleConstants.cancoderID, "Vulture");
         configAngleEncoder();
 
         /* Angle Motor Config */
-        mAngleMotor = new TalonFX(moduleConstants.angleMotorID);
+        mAngleMotor = new TalonFX(moduleConstants.angleMotorID, "Vulture");
         configAngleMotor();
 
         /* Drive Motor Config */
-        mDriveMotor = new TalonFX(moduleConstants.driveMotorID);
+        mDriveMotor = new TalonFX(moduleConstants.driveMotorID, "Vulture");
         configDriveMotor();
 
         lastAngle = getState().angle;
--- main/java/frc/robot/subsystems/Swerve.java
+++ main/java/frc/robot/subsystems/Swerve.java
@@ -24,7 +24,7 @@ public class Swerve extends SubsystemBase {
     public Pigeon2 gyro;
 
     public Swerve() {
-        gyro = new Pigeon2(Constants.Swerve.pigeonID);
+        gyro = new Pigeon2(Constants.Swerve.pigeonID, "Vulture");
         gyro.configFactoryDefault();
         zeroGyro();
 
